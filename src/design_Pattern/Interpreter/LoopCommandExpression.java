7cdd329540bd5e8b2cf9a5e284453654ca64b418
Songpenga <iiuuyy6654@gmail.com>
2023-07-23 01:19:48 +0900

    22. Interpreter : END [!ERROR : ActionCommandExpression.checkValidKeyword ]

--- src/design_Pattern/Interpreter/LoopCommandExpression.java
+++ src/design_Pattern/Interpreter/LoopCommandExpression.java
@@ -1,5 +1,44 @@
 package design_Pattern.Interpreter;
 
-public class LoopCommandExpression {
+public class LoopCommandExpression extends CommendExpression{
 
+	private int count;
+	private CommandListExpression expression;
+	
+	public LoopCommandExpression(String keyword) {
+		super(keyword);
+	}
+	@Override
+	public boolean parse(Context context) {
+		if(!checkValidKeyword(keyword)) return false;
+		String countKeyword = context.readNextKeyword();
+		if(countKeyword == null) return false;
+		
+		try {
+			count = Integer.parseInt(countKeyword);
+			
+			expression = new CommandListExpression();
+			if(context.readNextKeyword() == null) return false;
+			return expression.parse(context);
+		}catch(NumberFormatException e) {
+			return false;
+		}		
+	}
+	
+	public static boolean checkValidKeyword(String keyword) {
+		return keyword.equals("LOOP");
+	}
+	@Override
+	public boolean run() {
+		for(int i=0; i<count; i++) {
+			if(!expression.run()) {
+				return false;
+			}
+		}
+		return false;
+	}
+	
+	public String toString() {
+		return "LOOP(" + count + ")" + expression;
+	}
 }
--- src/design_Pattern/Interpreter/MainEntry.java
+++ src/design_Pattern/Interpreter/MainEntry.java
@@ -2,17 +2,16 @@ package design_Pattern.Interpreter;
 
 public class MainEntry {
 	public static void main(String[] args) {
-		String script = "BEGIN LOOG 2 BACK RIGHT END BACK END";
+		String script = "BEGIN FRONT LOOP 3 LOOP 2 RIGHT FRONT END LOOP 3 LEFT END BACK RIGHT END";
+		System.out.println(script);
 		
 		Context context = new Context(script);
+		Expression expression = new BeginExpression();
 		
-		while(true) {
-			String keyword = context.getCurrentKeyword();
-			if(keyword == null) break;
+		if(expression.parse(context)) {
+			System.out.println(expression);
 			
-			System.out.println(keyword);
-			
-			context.readNextKeyword();
+			expression.run();
 		}
 	}
 }
